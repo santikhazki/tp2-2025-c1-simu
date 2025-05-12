ee6e2a19a85b2183dfdf05cd0b3a453270acfe77
Pablo Fernandez <pablo@Mac-mini-de-Pablo.local>
2025-05-12 10:48:29 -0300

    - temp.js

--- temp.js
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import { findAllUsers } from "./src/data/userData.js";
-import { connectToDatabase } from "./src/data/connection.js";
-
-await connectToDatabase();
-await findAllUsers();
